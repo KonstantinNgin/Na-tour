1f19c0ae2fd882c03ff588cf7f71451de4c88e53
Konstantin Nogin <100676314+KonstantinNgin@users.noreply.github.com>
2023-02-02 21:40:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Бургер
+
+const iconMenu = document.querySelector('.navigation__icon');
+const menuBody = document.querySelector('.navigation__nav');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.navigation__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
